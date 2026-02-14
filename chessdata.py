1a86fc7d54cd3f252f18669612172328cbe9c924
Kamil Krecigłowa <2006.kamil.krec@gmail.com>
2026-02-14 18:53:20 +0100

    made the headers input all of the data, changed the structure for game storage for easier future analysis

--- chessdata.py
+++ chessdata.py
@@ -1,12 +1,45 @@
 #a code meant to analyze set of games from pgn files and giving feedback based on positions that need to be worked on
 Games={}
 class Cleangame():
-    def __init__(self,white,black,move):
+    def __init__(self,white,black,move,score):
         self.white=white
         self.black=black
         self.move=move
+        self.score=score
     def show(self):
-        return f"This is a game between: {self.white[0]} and {self.black[0]} and it went like this: \n {self.move}"
+        return f"This is a game between: {self.white[0]} and {self.black[0]} and it went like this: \n {self.move}. \nThe score? {self.score}"
+    class Move():
+        def __init__(self,piece,move,colour,takes):
+            self.piece=piece
+            self.move=move
+            self.colour=colour
+            self.takes=takes
+        def create(self,move,movenr):
+            takes=0
+            if move[0].islower(): piece="pawn"
+            else:
+                if move[0]=="N":
+                    piece="knight"
+                    move=move[1:]
+                elif move[0]=="B":
+                    piece="bishop"
+                    move=move[1:]
+                elif move[0]=="R":
+                    piece="rook"
+                    move=move[1:]
+                elif move[0]=="Q":
+                    piece="queen"
+                    move=move[1:]
+                elif move[0]=="K" or move[0]=="0":
+                    piece="king"
+                    move=move[1:]
+            #to turn into switch soon
+            if move[0]=="x":
+                takes=1
+                move=move[1:]
+            if movenr%2:colour="White"
+            else: colour="Black"
+            return Move(piece,move,colour,takes)
     def verifygame(self):
         pass
 def parse_games():
@@ -32,7 +65,7 @@ def parse_games():
             if line=="\n":
                 gamestart=False
                 moves=[a for a in moves if a]
-                Games[count]=Cleangame(headers["White"],headers["Black"],moves)
+                Games[count]=Cleangame(headers["White"],headers["Black"],moves[:-1],moves[-1])
                 moves=[]
                 headers.clear()
                 count+=1
