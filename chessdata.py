e1a22b857f8abd29ec3e77398b5fa7f09876c9a8
Kamil Krecigłowa <2006.kamil.krec@gmail.com>
2026-02-14 19:43:56 +0100

    Created working and lookable move history with side,piece,place of the move and information about captures and mates/checks

--- chessdata.py
+++ chessdata.py
@@ -7,41 +7,55 @@ class Cleangame():
         self.move=move
         self.score=score
     def show(self):
-        return f"This is a game between: {self.white[0]} and {self.black[0]} and it went like this: \n {self.move}. \nThe score? {self.score}"
-    class Move():
-        def __init__(self,piece,move,colour,takes):
-            self.piece=piece
-            self.move=move
-            self.colour=colour
-            self.takes=takes
-        def create(self,move,movenr):
-            takes=0
-            if move[0].islower(): piece="pawn"
-            else:
-                if move[0]=="N":
-                    piece="knight"
-                    move=move[1:]
-                elif move[0]=="B":
-                    piece="bishop"
-                    move=move[1:]
-                elif move[0]=="R":
-                    piece="rook"
-                    move=move[1:]
-                elif move[0]=="Q":
-                    piece="queen"
-                    move=move[1:]
-                elif move[0]=="K" or move[0]=="0":
-                    piece="king"
-                    move=move[1:]
-            #to turn into switch soon
-            if move[0]=="x":
-                takes=1
+        return f"This is a game between: {self.white[0]} and {self.black[0]} moves: {Cleangame.show_moves(self)}"
+    def show_moves(self):
+        movestring=""
+        for ply in self.move:
+            movestring+=ply.show()
+        return movestring
+class Move():
+    def __init__(self,piece,move,colour,takes):
+        self.piece=piece
+        self.move=move
+        self.colour=colour
+        self.takes=takes
+    def show(self):
+        if not self.takes:
+            return f"{self.colour} {self.piece} to {self.move}\n"
+        return f"{self.colour} {self.piece} takes on {self.move}\n"
+def createmove(move,movenr):
+    takes=0
+    print(move[0])
+    if move[0].islower(): piece="pawn"
+    else:
+        if move[0]=="N":
+            piece="knight"
+            move=move[1:]
+        elif move[0]=="B":
+            piece="bishop"
+            move=move[1:]
+        elif move[0]=="R":
+            piece="rook"
+            move=move[1:]
+        elif move[0]=="Q":
+            piece="queen"
+            move=move[1:]
+        elif move[0]=="K" or move[0]=="O":
+            piece="king"
+            if not move[0]=="0":
                 move=move[1:]
-            if movenr%2:colour="White"
-            else: colour="Black"
-            return Move(piece,move,colour,takes)
-    def verifygame(self):
-        pass
+    #to turn into switch soon
+    if "x" in move:
+        takes=1
+        move= move.split("x")[1]
+    if movenr%2:colour="White"
+    else: colour="Black"
+    return Move(piece,move,colour,takes)
+def separate(notation: list)-> list:
+    game=[]
+    for nr,move in enumerate(notation):
+        game.append(createmove(move,nr))
+    return game
 def parse_games():
     headers={}
     gamestart=False
@@ -65,14 +79,14 @@ def parse_games():
             if line=="\n":
                 gamestart=False
                 moves=[a for a in moves if a]
-                Games[count]=Cleangame(headers["White"],headers["Black"],moves[:-1],moves[-1])
+                game=separate(moves[:-1])
+                Games[count]=Cleangame(headers["White"],headers["Black"],game,moves[-1])
                 moves=[]
                 headers.clear()
                 count+=1
     for nr,game in Games.items():
         print(f"Game number {nr}\n{Games[nr].show()}")
-def separate():
-    pass
+
 def get():
     pass
 def main():
